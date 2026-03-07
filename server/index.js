27287a167d020f1bff3f533d18e7322a7aeb9fea
MARVAN <marvankp847@gmail.com>
2026-03-07 07:55:50 +0530

    Update MongoDB connection URI

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
 // MongoDB Connection
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb+srv://marvankp847_marvankp847_db_user:jD4cTT1aNrI3eVDE@cluster0.4dgc0s8.mongodb.net/ivault";
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb+srv://marvankp847_db_user:jD4cTT1aNrI3eVDE@cluster0.4dgc0s8.mongodb.net/ivault";
 
 mongoose.connect(MONGODB_URI)
     .then(() => console.log('Connected to MongoDB'))
